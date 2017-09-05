d28ce448dc5ecc2cb30b793f5e5052d00629ae6d
Alaa Rezk <a.rezk@shiftebusiness.com>
2017-09-05 15:35:17 +0200

    login error

---
+++ database/migrations/2017_09_05_121749_create_bids_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBidsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bids', function (Blueprint $table) {
+            $table->string('id')->primary()->index();;
+            $table->string('user_id');
+            $table->string('case_id');
+            $table->integer('bids_val');
+            $table->integer('is_pids')->default('0');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bids');
+    }
+}
